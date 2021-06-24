527d1ddcc675f582de856091eb5837ae726056fd
Ryan Morrison-Westphal <ryanmw@amazon.com>
2021-06-24 19:18:07 -0400

    smooth errors

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,21 +8,28 @@ fn main() {
 
     println!("Guess the number!");
 
-    println!("Please enter your guess:");
+    loop {
+        println!("Please enter your guess:");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read guess");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read guess");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
