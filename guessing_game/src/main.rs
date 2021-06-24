58304729e9d721471844906e2f0dbb3aa49d41c3
Ryan Morrison-Westphal <ryanmw@amazon.com>
2021-06-24 18:57:21 -0400

    guessing game with error crash

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,28 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is {}", secret_number);
+
+    println!("Guess the number!");
+
+    println!("Please enter your guess:");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read guess");
+
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+}
